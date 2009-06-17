da400dfbd2f24429811da924c5b326c97d4f6180
Jakob Schwendner <jakob@jakob-laptop.(none)>
2009-06-17 13:04:53 +0200

    added unit test infrastructure to cmake and included a template unit test.

---
+++ test/unit_test.cpp
@@ -0,0 +1,12 @@
+#include "envire/core.hpp"
+
+#define BOOST_TEST_MODULE EnvireTest 
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE( framenode ) 
+{
+   BOOST_CHECK( true ); 
+}
+
+// EOF
+//
