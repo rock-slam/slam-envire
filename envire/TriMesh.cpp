0068d2037b89799fbb6ecbbf5553390d04788b70
Jakob Schwendner <jakob@jakob-laptop.(none)>
2009-09-04 16:30:28 +0200

    added stubs for read/writeMap ply serialization

--- envire/TriMesh.cpp
+++ envire/TriMesh.cpp
@@ -1,7 +1,7 @@
 #include "Core.hpp"
 #include "TriMesh.hpp"
 
-#include <boost/shared_ptr.hpp>
+#include <stdexcept>
 
 using namespace envire;
 
@@ -28,4 +28,14 @@ TriMesh* TriMesh::clone()
     return new TriMesh(*this);
 }
 
+void TriMesh::writeMap(const std::string& path)
+{
+    // TODO write to ply file
+    throw std::runtime_error("not yet implemented");
+}
 
+void TriMesh::readMap(const std::string& path)
+{
+    // TODO read from ply file
+    throw std::runtime_error("not yet implemented");
+}
