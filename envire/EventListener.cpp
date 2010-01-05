752d174160fda058ea5ab0aed11b9a48ddce7ee1
Janosch Machowinski <janosch.machowinski@dfki.de>
2010-01-05 15:56:24 +0100

    added mission implementation of EventListener

---
+++ envire/EventListener.cpp
@@ -0,0 +1,44 @@
+#include "Core.hpp"
+
+namespace envire {
+  
+void EventListener::childAdded ( FrameNode* parent, FrameNode* child )
+{
+
+}
+
+void EventListener::childAdded ( Layer* parent, Layer* child )
+{
+
+}
+
+void EventListener::childRemoved ( FrameNode* parent, FrameNode* child )
+{
+
+}
+
+void EventListener::childRemoved ( Layer* parent, Layer* child )
+{
+
+}
+
+void EventListener::itemAttached ( EnvironmentItem* item )
+{
+
+}
+
+void EventListener::itemDetached ( EnvironmentItem* item )
+{
+
+}
+
+void EventListener::frameNodeSet(CartesianMap* map, FrameNode* node) 
+{
+}	
+
+void EventListener::itemModified ( EnvironmentItem* item )
+{
+
+}
+
+};
