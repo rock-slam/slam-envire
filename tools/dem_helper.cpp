ae21a54ee5232ca7f6893bbab00d84ac52541e28
Jakob Schwendner <jakob.schwendner@dfki.de>
2010-06-23 13:00:05 +0200

    this was not meant to be checked in

--- tools/dem_helper.cpp
+++ tools/dem_helper.cpp
@@ -18,23 +18,4 @@ int main(int argc, const char* argv[])
 
     Eigen::Vector3d v(12, 85, 0);
     cout << (q*(v*.5) + Eigen::Vector3d(-9, -3, -2 )).transpose() << endl;
-
-
-    envire::Serialization so;
-    boost::scoped_ptr<envire::Environment> env(so.unserialize( argv[1] ));
-
-    // update the operators
-    env->updateOperators();
-
-    ICP icp;
-
-    // and load all the pointcloud data into the icp model
-    std::vector<envire::TriMesh*> items = env->getItems<envire::TriMesh>();
-    for(std::vector<envire::TriMesh*>::iterator it=items.begin();it!=items.end();it++)
-    {
-	icp.updateTree( *it, 1.0 );
-    }
-
-
-    //Eigen::Translation3d(-9,-3,-2)*;
 }
