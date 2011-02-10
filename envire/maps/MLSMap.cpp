39f8fb9b956a65be91e0b3eb3e9e42ec3882e0d2
Jakob Schwendner <jakob.schwendner@dfki.de>
2011-02-10 17:28:03 +0100

    [mlsmap] extended interface and added some implementation

--- envire/maps/MLSMap.cpp
+++ envire/maps/MLSMap.cpp
@@ -7,13 +7,54 @@ Eigen::AlignedBox<double, 2> MLSMap::getExtents() const
     throw std::runtime_error("not implemented"); 
 }
 
-MLSMap::MLSMap(const MLSMap& other)
+MLSMap::MLSMap()
 {
-    // TODO implement
+}
+
+MLSMap::MLSMap(const MLSMap& other)
+    : grids( other.grids ), active( other.active )
+{
 }
 
 MLSMap& MLSMap::operator=(const MLSMap& other)
 {
-    // TODO implement
+    grids = other.grids;
+    active = other.active;
+
     return *this;
+}
+
+MultiLevelSurfaceGrid::SurfacePatch* 
+    MLSMap::getPatch( const Point& p, const SurfacePatch& patch, double sigma_threshold )
+{
+    // go backwards in the list, and try to find the point in any of the grids
+    for( std::vector<MultiLevelSurfaceGrid::Ptr>::reverse_iterator it = grids.rbegin(); it != grids.rend(); it++ )
+    {
+	MultiLevelSurfaceGrid::Ptr grid( *it );
+	Transform C_m2g = env->relativeTransform( getFrameNode(), grid->getFrameNode() );
+	MultiLevelSurfaceGrid::Position pos;
+	if( grid->toGrid((C_m2g * p).start<2>(), pos) )
+	{
+	    return grid->get( pos, patch, sigma_threshold );
+	}
+    }
+    return NULL;
+}
+
+void MLSMap::addGrid( MultiLevelSurfaceGrid::Ptr grid )
+{
+    grids.push_back( grid );
+    active = grid;
+}
+
+void MLSMap::createGrid( const Transform& trans )
+{
+    // don't do anything if there is no template 
+    if( !active )
+	return;
+
+    MultiLevelSurfaceGrid* grid_clone = active->cloneShallow(); 
+    FrameNode* fn = new FrameNode( trans );
+    env->addChild( active->getFrameNode(), fn );
+    env->setFrameNode( grid_clone, fn );
 }
--- envire/maps/MLSMap.hpp
+++ envire/maps/MLSMap.hpp
@@ -6,7 +6,7 @@
 namespace envire
 {
 
-class MLSMap : Map<2>
+class MLSMap : public Map<2>
 {
     ENVIRONMENT_ITEM( MLSMap )
 
@@ -22,12 +22,33 @@ public:
     Eigen::AlignedBox<double, 2> getExtents() const;
 
 public:
-    SurfacePatch* get( const Point& p, const SurfacePatch& patch, double sigma_threshold = 3.0 );
-    void updateCell( size_t m, size_t n, const SurfacePatch& patch );
+    /** get a patch from the stored grids. @param p is in the coordinate from of this map. */
+    SurfacePatch* getPatch( const Point& p, const SurfacePatch& patch, double sigma_threshold = 3.0 );
 
-protected:
+    /** 
+     * add new grid and make it active. The grid is assumed to be 
+     * attached to the environment. 
+     * @param grid - to be added to the map
+     */
+    void addGrid( MultiLevelSurfaceGrid::Ptr grid );
+
+    /** add a new grid taking the current active grid as a template
+     * and placing it relative to the active grid 
+     * @param trans - the transform between the active grid and the newly
+     *                created grid
+     */
+    void createGrid( const Transform& trans );
+
+    /** @return the currently active grid
+     */
+    MultiLevelSurfaceGrid::Ptr getActiveGrid() const { return active; }
+
+public:
+    // TODO we only store pointer to the grids here,
+    // there is a little problem here, since we actually should
+    // also add a framenode per grid
     std::vector<MultiLevelSurfaceGrid::Ptr> grids;
-
+    MultiLevelSurfaceGrid::Ptr active;
 };
 
 }
