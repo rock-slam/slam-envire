d4454a57066b152ee5bb1d9bab402a1eb22f1e49
Jakob Schwendner <jakob.schwendner@dfki.de>
2011-03-25 16:50:30 +0100

    viz: updated uncertainty to also show the uncertainty ellipses (1 sigma)

--- viz/Uncertainty.hpp
+++ viz/Uncertainty.hpp
@@ -4,6 +4,7 @@
 #include <boost/random/linear_congruential.hpp>
 #include <boost/random/uniform_real.hpp>
 #include <boost/random/variate_generator.hpp>
+#include <boost/random/normal_distribution.hpp>
 
 #include <osg/PositionAttitudeTransform>
 #include <Eigen/Core>
@@ -16,7 +17,9 @@ class Uncertainty : public osg::PositionAttitudeTransform
 public:
     Uncertainty();
 
+    void setMean( const Eigen::Vector2d& mean );
     void setMean( const Eigen::Vector3d& mean );
+    void setCovariance( const Eigen::Matrix2d& cov );
     void setCovariance( const Eigen::Matrix3d& cov );
 
     void showSamples() { m_showSamples = true; }
@@ -25,13 +28,19 @@ public:
     void setNumSamples(size_t samples) { num_samples = samples; }
 
 private:
+    void redraw( int dim );
+    void addEllipse( int axis );
+    void addSamples();
+
     /** random number generator */
     boost::minstd_rand rand_gen;
+    boost::variate_generator<boost::minstd_rand&, boost::normal_distribution<> > rand_norm;
 
     bool m_showSamples;
     osg::ref_ptr<osg::Geode> geode;
 
     size_t num_samples;
+    int dim;
 };
 
 }
