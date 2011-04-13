a3e0933dc93140d7ea732f31ca7d5b3d5a977684
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-04-13 18:12:15 +0200

    tools: add env_update
    It simply loads an environment and re-runs all operators

---
+++ tools/env_update.cpp
@@ -0,0 +1,22 @@
+#include "envire/Core.hpp"
+#include "boost/scoped_ptr.hpp"
+
+using namespace envire;
+using namespace std;
+     
+int main( int argc, char* argv[] )
+{
+    if( argc != 2 ) 
+    {
+        std::cout << "usage: env_update <path_to_env>\n"
+            << "  loads the specified environment and update all the generated maps\n"
+            << "  (i.e. runs all operators)\n"
+            << std::endl;
+        exit(1);
+    }
+
+    boost::scoped_ptr<envire::Environment> env(Environment::unserialize(argv[1]));
+    env->updateOperators();
+    env->serialize(argv[1]);
+}
+
