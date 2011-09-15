0c5c94bd447acde086eb0104dda5fedfbf544706
Jakob Schwendner <jakob.schwendner@dfki.de>
2011-09-15 15:40:01 +0200

    features: add original image point to keypoint

--- src/core/Features.hpp
+++ src/core/Features.hpp
@@ -1,6 +1,8 @@
 #ifndef __ENVIRE_FEATURES_HPP__
 #define __ENVIRE_FEATURES_HPP__
 
+#include <base/eigen.h>
+
 namespace envire
 {
 
@@ -13,7 +15,12 @@ struct KeyPoint
 {
     typedef float Scalar;
 
+    /** the original image point */
+    base::Vector2d point;
+
+    /** scaled size of the feature */
     Scalar size;
+
     Scalar angle;
     Scalar response;
 };
