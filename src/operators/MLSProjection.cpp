bb7dd722d37c7b70984e081581d267c2abffa98d
Jakob Schwendner <jakob.schwendner@dfki.de>
2012-03-15 14:33:48 +0100

    first shot at handling negative information in mlsprojection

--- src/operators/MLSProjection.cpp
+++ src/operators/MLSProjection.cpp
@@ -2,6 +2,8 @@
 #include <set>
 #include <Eigen/LU>
 
+#include <envire/tools/BresenhamLine.hpp>
+
 using namespace envire;
 
 ENVIRONMENT_ITEM_DEF( MLSProjection )
@@ -37,13 +39,6 @@ void MLSProjection::addOutput( MultiLevelSurfaceGrid* grid )
 
 void MLSProjection::projectPointcloudWithUncertainty( envire::MultiLevelSurfaceGrid* grid, envire::Pointcloud* pc )
 {
-    // this method also incorporates the uncertainty in the transform
-    // instead of just the uncertainty from the pointcloud items
-    // this is done by transforming the pointcloud to temporary mls
-    // grid first, and then "smearing" the uncertainty over the already
-    // indexed cells. This is mainly for performance issues, since calculating
-    // the transform for each input point is much more costly.
-
     // create a new grid with the same dimensions in case the given grid is not
     // empty
     boost::intrusive_ptr<envire::MultiLevelSurfaceGrid> t_grid;
@@ -61,10 +56,17 @@ void MLSProjection::projectPointcloudWithUncertainty( envire::MultiLevelSurfaceG
 	    pc->getFrameNode(), grid->getFrameNode() );
     Eigen::Affine3d C_g2m( C_m2g.getTransform().inverse( Eigen::Isometry ) );
 
+    // get the origin of the poincloud as a map cell
+    Eigen::Vector3d origin_m = C_m2g.getTransform() * Eigen::Vector3d::Zero();
+    GridBase::Position origin;
+    if( !grid->toGrid( origin_m.head<2>(), origin ) )
+	if( m_negativeInformation )
+	    throw std::runtime_error( "origin of pointcloud needs to be within grid." );
+
+    // go through all the cells that have been touched
     typedef MultiLevelSurfaceGrid::Position position;
     std::set<position> &cells = t_grid->getIndex()->cells;
 
-    // go through all the cells that have been touched
     for(std::set<position>::iterator it = cells.begin(); it != cells.end(); it++)
     {
 	const size_t xi = it->x;
@@ -72,17 +74,27 @@ void MLSProjection::projectPointcloudWithUncertainty( envire::MultiLevelSurfaceG
 
 	for(MultiLevelSurfaceGrid::iterator cit = t_grid->beginCell(xi,yi); cit != t_grid->endCell(); cit++ )
 	{
-	    // get center of cell
-	    double x, y;
-	    t_grid->fromGrid( xi, yi, x, y );
-	    Eigen::Vector3d cellcenter = C_g2m * Eigen::Vector3d(x, y, cit->mean);
+	    if( withUncertainty )
+	    {
+		// this method also incorporates the uncertainty in the transform
+		// instead of just the uncertainty from the pointcloud items
+		// this is done by transforming the pointcloud to temporary mls
+		// grid first, and then "smearing" the uncertainty over the already
+		// indexed cells. This is mainly for performance issues, since calculating
+		// the transform for each input point is much more costly.
 
-	    // use the cells stdev for the point, this is not quite exact, but should do 
-	    const double p_var = cit->stdev * cit->stdev;
-	    PointWithUncertainty p = C_m2g * PointWithUncertainty( cellcenter, Eigen::Matrix3d::Zero() );
+		// get center of cell
+		double x, y;
+		t_grid->fromGrid( xi, yi, x, y );
+		Eigen::Vector3d cellcenter = C_g2m * Eigen::Vector3d(x, y, cit->mean);
 
-	    // write the transformed uncertainty back
-	    cit->stdev = sqrt(p_var + p.getCovariance()(2,2));
+		// use the cells stdev for the point, this is not quite exact, but should do 
+		const double p_var = cit->stdev * cit->stdev;
+		PointWithUncertainty p = C_m2g * PointWithUncertainty( cellcenter, Eigen::Matrix3d::Zero() );
+
+		// write the transformed uncertainty back
+		cit->stdev = sqrt(p_var + p.getCovariance()(2,2));
+	    }
 
 	    // add the patch with the updated uncertainty into the target grid
 	    // TODO: in case we are operating on the target grid already, 
@@ -90,6 +102,42 @@ void MLSProjection::projectPointcloudWithUncertainty( envire::MultiLevelSurfaceG
 	    // variance we might have some patches that actually belong together.
 	    if( t_grid != grid )
 		grid->updateCell( xi, yi, *cit );
+
+	    if( m_negativeInformation )
+	    {
+		// in order to handle negative information (e.g. knowledge that
+		// a cell is free), we use bresenhams line algorithm to find the cells
+		// from each known cells to the origin. For each of these cells, 
+		// we add absence information
+		
+		// this is the distance on the x/y plane from origin to 
+		// the current cell
+		const double plane_dist = (grid->fromGrid( origin ) - grid->fromGrid( *it )).norm();
+
+		// this is the height difference between the origin and the cell
+		const double plane_z = origin_m.z() - cit->mean;
+
+		// this is the maximum height of the negative cell,
+		// which joins the height of the cell, and how high it
+		// is perceived from the origin point of view
+		const double height = 
+		    fabs( grid->getScaleX()/2.0 / plane_dist * plane_z ) + cit->height;
+
+		std::vector<GridBase::Position> line_cells;
+		lineBresenham( *it, origin, line_cells );
+
+		for( size_t li = 0; li < line_cells.size(); li++ )
+		{
+		    const double factor = (li * 1.0 / line_cells.size());
+		    const double z_mean = cit->mean + plane_z * factor;
+		    const double z_stdev = cit->stdev * (1.0 - factor); 
+		    const double p_height = height * (1.0 - factor);
+
+		    MLSGrid::SurfacePatch np( z_mean, z_stdev, p_height, MLSGrid::SurfacePatch::NEGATIVE );
+		    
+		    t_grid->updateCell( line_cells[li], np );
+		}
+	    }
 	}
     }
 }
---
+++ src/tools/BresenhamLine.hpp
@@ -0,0 +1,176 @@
+#ifndef ENVIRE_BRESENHAM_LINE__
+#define ENVIRE_BRESENHAM_LINE__
+
+#include <envire/maps/GridBase.hpp>
+
+namespace envire
+{
+
+/**
+ * Draws a line between two points p1(p1x,p1y) and p2(p2x,p2y).
+ * This function is based on the Bresenham's line algorithm and is highly 
+ * optimized to be able to draw lines very quickly. There is no floating point 
+ * arithmetic nor multiplications and divisions involved. Only addition, 
+ * subtraction and bit shifting are used. 
+ *
+ * Note that you have to define your own customized setPixel(x,y) function, 
+ * which essentially lights a pixel on the screen.
+ *
+ * Author: Woon Khang Tang
+ * url: http://www.codekeep.net/snippets/e39b2d9e-0843-4405-8e31-44e212ca1c45.aspx
+ * (c) All public snippets in CodeKeep may be freely used, copied, and distributed. 
+ */
+void lineBresenham(const envire::GridBase::Position& p1, const envire::GridBase::Position& p2, std::vector<envire::GridBase::Position>& positions )
+{
+    int p1x = p1.x, p1y = p1.y;
+    int p2x = p2.x, p2y = p2.y;
+    int F, x, y;
+
+    if (p1x > p2x)  // std::swap points if p1 is on the right of p2
+    {
+        std::swap(p1x, p2x);
+        std::swap(p1y, p2y);
+    }
+
+    // Handle trivial cases separately for algorithm speed up.
+    // Trivial case 1: m = +/-INF (Vertical line)
+    if (p1x == p2x)
+    {
+        if (p1y > p2y)  // std::swap y-coordinates if p1 is above p2
+        {
+            std::swap(p1y, p2y);
+        }
+
+        x = p1x;
+        y = p1y;
+        while (y <= p2y)
+        {
+	    positions.push_back( envire::GridBase::Position( x, y ) );
+            y++;
+        }
+        return;
+    }
+    // Trivial case 2: m = 0 (Horizontal line)
+    else if (p1y == p2y)
+    {
+        x = p1x;
+        y = p1y;
+
+        while (x <= p2x)
+        {
+	    positions.push_back( envire::GridBase::Position( x, y ) );
+            x++;
+        }
+        return;
+    }
+
+
+    int dy            = p2y - p1y;  // y-increment from p1 to p2
+    int dx            = p2x - p1x;  // x-increment from p1 to p2
+    int dy2           = (dy << 1);  // dy << 1 == 2*dy
+    int dx2           = (dx << 1);
+    int dy2_minus_dx2 = dy2 - dx2;  // precompute constant for speed up
+    int dy2_plus_dx2  = dy2 + dx2;
+
+
+    if (dy >= 0)    // m >= 0
+    {
+        // Case 1: 0 <= m <= 1 (Original case)
+        if (dy <= dx)   
+        {
+            F = dy2 - dx;    // initial F
+
+            x = p1x;
+            y = p1y;
+            while (x <= p2x)
+            {
+		positions.push_back( envire::GridBase::Position( x, y ) );
+                if (F <= 0)
+                {
+                    F += dy2;
+                }
+                else
+                {
+                    y++;
+                    F += dy2_minus_dx2;
+                }
+                x++;
+            }
+        }
+        // Case 2: 1 < m < INF (Mirror about y=x line
+        // replace all dy by dx and dx by dy)
+        else
+        {
+            F = dx2 - dy;    // initial F
+
+            y = p1y;
+            x = p1x;
+            while (y <= p2y)
+            {
+		positions.push_back( envire::GridBase::Position( x, y ) );
+                if (F <= 0)
+                {
+                    F += dx2;
+                }
+                else
+                {
+                    x++;
+                    F -= dy2_minus_dx2;
+                }
+                y++;
+            }
+        }
+    }
+    else    // m < 0
+    {
+        // Case 3: -1 <= m < 0 (Mirror about x-axis, replace all dy by -dy)
+        if (dx >= -dy)
+        {
+            F = -dy2 - dx;    // initial F
+
+            x = p1x;
+            y = p1y;
+            while (x <= p2x)
+            {
+		positions.push_back( envire::GridBase::Position( x, y ) );
+                if (F <= 0)
+                {
+                    F -= dy2;
+                }
+                else
+                {
+                    y--;
+                    F -= dy2_plus_dx2;
+                }
+                x++;
+            }
+        }
+        // Case 4: -INF < m < -1 (Mirror about x-axis and mirror 
+        // about y=x line, replace all dx by -dy and dy by dx)
+        else    
+        {
+            F = dx2 + dy;    // initial F
+
+            y = p1y;
+            x = p1x;
+            while (y >= p2y)
+            {
+		positions.push_back( envire::GridBase::Position( x, y ) );
+                if (F <= 0)
+                {
+                    F += dx2;
+                }
+                else
+                {
+                    x++;
+                    F += dy2_plus_dx2;
+                }
+                y--;
+            }
+        }
+    }
+}
+
+}
+
+#endif
