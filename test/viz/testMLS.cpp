b4935a114e29a3de30241a4aab9ce5cf56a0b099
Jakob Schwendner <jakob.schwendner@dfki.de>
2012-03-16 15:21:39 +0100

    mls: updated test

--- test/viz/testMLS.cpp
+++ test/viz/testMLS.cpp
@@ -87,7 +87,7 @@ BOOST_AUTO_TEST_CASE( mlsnegative_test )
     env->getRootNode()->addChild( pcfn );
     pc->setFrameNode( pcfn );
 
-    MultiLevelSurfaceGrid *mls = new MultiLevelSurfaceGrid(100, 100, 0.5, 0.5, -5, -5);
+    MultiLevelSurfaceGrid *mls = new MultiLevelSurfaceGrid(100, 100, 0.5, 0.5, -25, -25);
     env->attachItem( mls );
     FrameNode *mlsfn = new FrameNode( Eigen::Affine3d( Eigen::Translation3d( 0, 0, 0 ) ) );
     env->getRootNode()->addChild( mlsfn );
@@ -113,47 +113,15 @@ BOOST_AUTO_TEST_CASE( mlsnegative_test )
 
     mlsp->updateAll();
 
-    while( app.isRunning() );
+    MLSGrid::SurfacePatch p1( 0.5, 0.1, 0.0, MLSGrid::SurfacePatch::HORIZONTAL );
+    p1.update_idx = 1;
+    mls->updateCell( 50, 51, p1 );
 
+    MLSGrid::SurfacePatch p2( 0.1, 0.1, 1.0, MLSGrid::SurfacePatch::NEGATIVE );
+    p2.update_idx = 1;
+    mls->updateCell( 50, 52, p2 );
 
-    /*
-    env->getRootNode()->addChild( fm );
-    mls->setFrameNode( fm );
-    for( int i=0; i<100; i++ )
-    {
-	for( int j=0; j<100; j++ )
-	{
-	    double h = 0.0;
-	    mls->insertTail( i, j, MultiLevelSurfaceGrid::SurfacePatch( h, 0.05 ) );
-	}
-    }
     mls->itemModified();
 
-    MultiLevelSurfaceGrid *mls2 = new MultiLevelSurfaceGrid(100, 100, 0.1, 0.1);
-    env->attachItem( mls2 );
-
-    FrameNode *fm2 = fm->clone();
-    env->getRootNode()->addChild( fm2 );
-    mls2->setFrameNode( fm2 );
-
-    for(int i=0;i<500 && app.isRunning();i++)
-    {
-	mls2->clear();
-	for( int m=48; m<52; m++ )
-	{
-	    for( int n=40; n<60; n++ )
-	    {
-		double r = i * 0.02;
-		double h = std::max( sin(m*0.1+n*0.2), r  );
-		mls2->insertTail( m, n, MultiLevelSurfaceGrid::SurfacePatch( h, 0.05 ) );
-	    }
-	}
-	mls2->itemModified();
-	std::pair<double, double> res = mls->matchHeight( *mls2 );
-	std::cout << "diff : " << res.first << " var: " << res.second << std::endl;
-	fm2->setTransform( Eigen::Affine3d( Eigen::Translation3d( 0, 0, -res.first ) ) );
-
-	usleep( 1000*1000 );
-    }
-    */
+    while( app.isRunning() );
 }
