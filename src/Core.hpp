27cd8aa4f26ef84d69e67596fef3c3f5ddfbc447
Jakob Schwendner <jakob.schwendner@dfki.de>
2012-04-19 12:27:26 +0200

    moved Holder out of Core.hpp

--- src/Core.hpp
+++ src/Core.hpp
@@ -15,8 +15,8 @@
 
 #include <envire/core/EventSource.hpp>
 #include <envire/core/EventTypes.hpp>
+#include <envire/core/Holder.hpp>
 #include <base/samples/rigid_body_state.h>
-#include <boost/noncopyable.hpp>
 
 #define ENVIRONMENT_ITEM_DEF( _classname ) \
 const std::string _classname::className = "envire::" #_classname; \
@@ -58,74 +58,6 @@ namespace envire
     class EventHandler;
     class Event;
     class SerializationFactory;
-
-    /** Baseclass for generically holding pointer to objects, while still
-     * ensuring, that the destructor of that object is called, when the holder
-     * object is destructed.
-     */
-    class HolderBase
-    {
-    public:
-	virtual ~HolderBase() {};
-
-        template <typename T> bool isOfType() const;
-	template <typename T> T& get();
-    	template <typename T> const T& get() const;
-	virtual HolderBase* clone() const = 0;
-    };
-
-    /** Templated holder class, that will construct an object of type T,
-     * provide access to it, and also delete the object again when it is
-     * destroyed.
-     */
-    template <typename T>
-	class Holder : public HolderBase, boost::noncopyable 
-    {
-	T* ptr;
-
-    public:
-	Holder()
-	{
-	    ptr = new T();
-	};
-
-	explicit Holder( T* ptr )
-	    : ptr( ptr )
-	{
-	}
-
-	~Holder()
-	{
-	    delete ptr;
-	};
-
-	T* getData() const
-	{
-	    return ptr;
-	}
-
-	Holder<T>* clone() const
-	{
-	    T* clone = new T(*ptr);
-	    return new Holder<T>(clone);
-	}
-    };
-
-    template <typename T> bool HolderBase::isOfType() const
-    {
-        Holder<T> const* myself = dynamic_cast< Holder<T> const* >(this);
-        return myself ? true : false;
-    }
-    template <typename T> T& HolderBase::get()
-    {
-        return *dynamic_cast< Holder<T>* >(this)->getData();
-    }
-
-    template <typename T> 
-    const T& HolderBase::get() const
-    {
-        return *dynamic_cast< Holder<T>* >(this)->getData();
-    }
 
     /** Base class for alle items that are defined in the envire framework.
      * Mainly handles the unique_id feature and the pointer to the environment
