55aa23dafe6295e738ab9bafcac706a59dd04f77
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-04-23 09:43:03 +0200

    viz: register the visualizer for ReadOnlyPointer as well

--- viz/vizkit_plugin.rb
+++ viz/vizkit_plugin.rb
@@ -1,3 +1,4 @@
 Vizkit::UiLoader.register_3d_plugin('envire::EnvireVisualization', 'envire', 'EnvireVisualization')
 Vizkit::UiLoader.register_3d_plugin_for('envire::EnvireVisualization', "/envire/BinaryEvent", :updateBinaryEvent )
 Vizkit::UiLoader.register_3d_plugin_for('envire::EnvireVisualization', "/std/vector</envire/BinaryEvent>", :updateBinaryEvents )
+Vizkit::UiLoader.register_3d_plugin_for('envire::EnvireVisualization', "/RTT/extras/ReadOnlyPointer</std/vector</envire/BinaryEvent>>", :updateBinaryEvents )
