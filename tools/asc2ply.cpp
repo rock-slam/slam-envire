c3f9dfa9252211a2cca0c9cb57dcfdb8f1ca3f3a
Jakob Schwendner <jakob.schwendner@dfki.de>
2012-04-27 10:35:03 +0200

    tools: added option for voxel based sampling to asc2ply

--- tools/asc2ply.cpp
+++ tools/asc2ply.cpp
@@ -1,5 +1,8 @@
 #include "envire/Core.hpp"
 #include "envire/maps/Pointcloud.hpp"
+#ifdef ENVIRE_USE_CGAL
+#include "envire/operators/SimplifyPointcloud.hpp"
+#endif
 
 #include "boost/scoped_ptr.hpp"
 #include <boost/version.hpp>
@@ -13,7 +16,7 @@ using boost::lexical_cast;
      
 void usage()
 {
-    std::cout << "usage: asc2ply asc_file ply_file [sampling]" << std::endl;
+    std::cout << "usage: asc2ply asc_file ply_file [sampling] [cell_size]" << std::endl;
 }
 
 int main( int argc, char* argv[] )
@@ -27,12 +30,40 @@ int main( int argc, char* argv[] )
     string asc_file = argv[1];
     string ply_file = argv[2];
     int sampling = 1;
+    float cell_size = 0;
     if( argc >= 4 )
 	sampling = lexical_cast<int>( argv[3] );
+    if( argc >= 5 )
+	cell_size = lexical_cast<float>( argv[4] );
+
+    Environment env;
 
     Pointcloud::Ptr pc = new Pointcloud();
+    env.attachItem( pc.get() );
+    env.setFrameNode( pc.get(), env.getRootNode() );
     ifstream asc( asc_file.c_str() );
     pc->readText( asc, sampling, Pointcloud::XYZR );
+
+    if( cell_size > 0 )
+    {
+#ifdef ENVIRE_USE_CGAL
+	Pointcloud::Ptr pc_simp = new Pointcloud();
+	env.attachItem( pc_simp.get() );
+	env.setFrameNode( pc_simp.get(), env.getRootNode() );
+
+	SimplifyPointcloud::Ptr spc = new SimplifyPointcloud();
+	env.attachItem( spc.get() );
+	spc->addInput( pc.get() );
+	spc->addOutput( pc_simp.get() );
+	spc->setSimplifyCellSize( cell_size );
+
+	spc->updateAll();
+
+	pc = pc_simp;
+#else
+	std::cout << "ignoring cell_size parameter since CGAL is not compiled in." << std::endl;
+#endif
+    }
 
     ofstream ply( ply_file.c_str() );
     pc->writePly( ply_file, ply );
