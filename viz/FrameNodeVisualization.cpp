8fe9d569343ac08c46c9de263fb4bcd1d5c8acc2
Jakob Schwendner <jakob.schwendner@dfki.de>
2012-05-31 09:55:42 +0200

    Show uncertainty for framenodes with uncertainty

--- viz/FrameNodeVisualization.cpp
+++ viz/FrameNodeVisualization.cpp
@@ -6,13 +6,13 @@
 #include <osg/ShapeDrawable>
 #include <osg/Geode>
 #include <osg/Geometry>
+#include <vizkit/Uncertainty.hpp>
 
 namespace envire {
 
 osg::Group* FrameNodeVisualization::getNodeForItem(envire::EnvironmentItem* item) const
 {
     osg::PositionAttitudeTransform *transform = new osg::PositionAttitudeTransform;
-
     updateNode(item, transform);
     
     return transform;
@@ -23,18 +23,54 @@ bool FrameNodeVisualization::handlesItem(envire::EnvironmentItem* item) const
     return dynamic_cast<envire::FrameNode *>(item);
 }
 
+void setTransform( osg::PositionAttitudeTransform *transform, const Eigen::Affine3d& trans )
+{
+    const Eigen::Quaterniond quat(trans.linear());
+    const Eigen::Vector3d translation = trans.translation();
+    
+    transform->setPosition(osg::Vec3f( translation.x(), translation.y(), translation.z()));
+    transform->setAttitude(osg::Quat( quat.x(), quat.y(), quat.z(), quat.w()));
+}
+
 void FrameNodeVisualization::updateNode(envire::EnvironmentItem* item, osg::Group* group) const
 {
     envire::FrameNode *fn = dynamic_cast<envire::FrameNode *>(item);
     osg::PositionAttitudeTransform *transform = dynamic_cast<osg::PositionAttitudeTransform *>(group);
     
     assert(fn && transform);
+    setTransform( transform, fn->getTransform() );
     
-    const Eigen::Quaterniond quat(fn->getTransform().rotation());
-    const Eigen::Vector3d translation = fn->getTransform().translation();
-    
-    transform->setPosition(osg::Vec3f( translation.x(), translation.y(), translation.z()));
-    transform->setAttitude(osg::Quat( quat.x(), quat.y(), quat.z(), quat.w()));
+    // slightly awkward way of adding the uncertainty,
+    // since it should actually be added to the parent
+    // frame. adds a group with the inverse transform to 
+    // emulate the same effect
+    if( fn->getTransformWithUncertainty().hasUncertainty() )
+    {
+	const std::string name = "FrameNodeUncertaintyGroup";
+	osg::PositionAttitudeTransform *ug = NULL;
+	for( size_t i=0; i<group->getNumChildren(); ++i )
+	{
+	    if( group->getChild(i)->getName() == name )
+	       ug = dynamic_cast<osg::PositionAttitudeTransform*>(group->getChild(i));
+	}
+	if( !ug )
+	{
+	    // use the inverse transform
+	    ug = new osg::PositionAttitudeTransform();
+	    ug->setName( name );
+	    group->addChild( ug );
+	}
+	setTransform( ug, fn->getTransform().inverse() );
+
+	ug->removeChildren(0, ug->getNumChildren());
+
+	TransformWithUncertainty tf = fn->getTransformWithUncertainty();
+	vizkit::Uncertainty *ellipse = new vizkit::Uncertainty();
+	ellipse->setMean( Eigen::Vector3d(tf.getTransform().translation()) );
+	ellipse->setCovariance( Eigen::Matrix3d( tf.getCovariance().bottomRightCorner<3,3>()) );
+
+	ug->addChild( ellipse );
+    }
 }
 
 void FrameNodeVisualization::highlightNode(envire::EnvironmentItem* item, osg::Group* group) const
