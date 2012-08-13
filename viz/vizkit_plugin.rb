0bee90fc283c1334620a64bc0771a56dc36042f0
Alexander Duda <Alexander@Duda.dfki.de>
2012-08-13 12:57:40 +0200

    remove register widget for ReadOnlyPointer
    this should be automatically handled by vizkit.rb

--- viz/vizkit_plugin.rb
+++ viz/vizkit_plugin.rb
@@ -1,4 +1,3 @@
 Vizkit::UiLoader.register_3d_plugin('EnvireVisualization', 'envire', 'EnvireVisualization')
 Vizkit::UiLoader.register_3d_plugin_for('EnvireVisualization', "/envire/BinaryEvent", :updateBinaryEvent )
 Vizkit::UiLoader.register_3d_plugin_for('EnvireVisualization', "/std/vector</envire/BinaryEvent>", :updateBinaryEvents )
-Vizkit::UiLoader.register_3d_plugin_for('EnvireVisualization', "/RTT/extras/ReadOnlyPointer</std/vector</envire/BinaryEvent>>", :updateBinaryEvents )
