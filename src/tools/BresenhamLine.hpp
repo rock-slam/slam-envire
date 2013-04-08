dce043b5789c95e604532ebde7bd337d7d092ca0
Janosch Machowinski <janosch.machowinski@dfki.de>
2013-04-08 10:55:09 +0200

    replaced Bresenham implementation
    The old implementation switched start and goal in some cases.
    This was an unwanted behaviour. The new version may also be
    used incrementally.

--- src/tools/BresenhamLine.hpp
+++ src/tools/BresenhamLine.hpp
@@ -7,171 +7,54 @@ namespace envire
 {
 
 /**
- * Draws a line between two points p1(p1x,p1y) and p2(p2x,p2y).
- * This function is based on the Bresenham's line algorithm and is highly 
- * optimized to be able to draw lines very quickly. There is no floating point 
- * arithmetic nor multiplications and divisions involved. Only addition, 
- * subtraction and bit shifting are used. 
- *
- * Note that you have to define your own customized setPixel(x,y) function, 
- * which essentially lights a pixel on the screen.
- *
- * Author: Woon Khang Tang
- * url: http://www.codekeep.net/snippets/e39b2d9e-0843-4405-8e31-44e212ca1c45.aspx
- * (c) All public snippets in CodeKeep may be freely used, copied, and distributed. 
- *
- * @todo update to iterator like accessor
- */
-void lineBresenham(const envire::GridBase::Position& p1, const envire::GridBase::Position& p2, std::vector<envire::GridBase::Position>& positions )
-{
-    int p1x = p1.x, p1y = p1.y;
-    int p2x = p2.x, p2y = p2.y;
-    int F, x, y;
+* Convenience method that returns all points on a line between p1 and p2.
+* */
+void lineBresenham(const envire::GridBase::Position& p1, const envire::GridBase::Position& p2, std::vector<envire::GridBase::Position>& positions );
 
-    if (p1x > p2x)  // std::swap points if p1 is on the right of p2
-    {
-        std::swap(p1x, p2x);
-        std::swap(p1y, p2y);
-    }
+/**
+ * This class use usefull for calculation
+ * straight lines in grid. The Bresenham Algrorithm 
+ * is a fast but inaccurate way to do this. 
+ * Inaccurate means, that the result will have
+ * aliasing artifacts.
+ * 
+ * The implementation of the core algorithm was taken from an wikipedia article.
+ * */
+class Bresenham {
+    public:
+        Bresenham(const Eigen::Vector2i &start, const Eigen::Vector2i &end);
+        Bresenham(const GridBase::Position& start, const GridBase::Position& end);
+        
+        /**
+         * Inits the algorithm.
+         * The method may be used to 'reinit' the class
+         * after a line was allready interpolated. 
+         * */
+        void init(const GridBase::Position& start, const GridBase::Position& end);
+        void init(int startX, int startY, int endX, int endY);
 
-    // Handle trivial cases separately for algorithm speed up.
-    // Trivial case 1: m = +/-INF (Vertical line)
-    if (p1x == p2x)
-    {
-        if (p1y > p2y)  // std::swap y-coordinates if p1 is above p2
-        {
-            std::swap(p1y, p2y);
-        }
+        /**
+         * Calculates the next point in the line 
+         * and returns it over the given paramters.
+         * 
+         * returns false if the end point is reached an
+         *              no further point can be calculated.
+         * 
+         * */
+        bool getNextPoint(int &x, int &y);
+        bool getNextPoint(Eigen::Vector2i &p);
+        bool getNextPoint(GridBase::Position &p);
+        
+        /**
+         * Convenience method that returns all points on a line.
+         * */
+        void getLine(std::vector<envire::GridBase::Position>& positions);
+    private:
+        bool hasP;
+        int x0,y0, x1, y1, dx, sx, dy, sy, err, e2;
 
-        x = p1x;
-        y = p1y;
-        while (y <= p2y)
-        {
-	    positions.push_back( envire::GridBase::Position( x, y ) );
-            y++;
-        }
-        return;
-    }
-    // Trivial case 2: m = 0 (Horizontal line)
-    else if (p1y == p2y)
-    {
-        x = p1x;
-        y = p1y;
+};
 
-        while (x <= p2x)
-        {
-	    positions.push_back( envire::GridBase::Position( x, y ) );
-            x++;
-        }
-        return;
-    }
-
-
-    int dy            = p2y - p1y;  // y-increment from p1 to p2
-    int dx            = p2x - p1x;  // x-increment from p1 to p2
-    int dy2           = (dy << 1);  // dy << 1 == 2*dy
-    int dx2           = (dx << 1);
-    int dy2_minus_dx2 = dy2 - dx2;  // precompute constant for speed up
-    int dy2_plus_dx2  = dy2 + dx2;
-
-
-    if (dy >= 0)    // m >= 0
-    {
-        // Case 1: 0 <= m <= 1 (Original case)
-        if (dy <= dx)   
-        {
-            F = dy2 - dx;    // initial F
-
-            x = p1x;
-            y = p1y;
-            while (x <= p2x)
-            {
-		positions.push_back( envire::GridBase::Position( x, y ) );
-                if (F <= 0)
-                {
-                    F += dy2;
-                }
-                else
-                {
-                    y++;
-                    F += dy2_minus_dx2;
-                }
-                x++;
-            }
-        }
-        // Case 2: 1 < m < INF (Mirror about y=x line
-        // replace all dy by dx and dx by dy)
-        else
-        {
-            F = dx2 - dy;    // initial F
-
-            y = p1y;
-            x = p1x;
-            while (y <= p2y)
-            {
-		positions.push_back( envire::GridBase::Position( x, y ) );
-                if (F <= 0)
-                {
-                    F += dx2;
-                }
-                else
-                {
-                    x++;
-                    F -= dy2_minus_dx2;
-                }
-                y++;
-            }
-        }
-    }
-    else    // m < 0
-    {
-        // Case 3: -1 <= m < 0 (Mirror about x-axis, replace all dy by -dy)
-        if (dx >= -dy)
-        {
-            F = -dy2 - dx;    // initial F
-
-            x = p1x;
-            y = p1y;
-            while (x <= p2x)
-            {
-		positions.push_back( envire::GridBase::Position( x, y ) );
-                if (F <= 0)
-                {
-                    F -= dy2;
-                }
-                else
-                {
-                    y--;
-                    F -= dy2_plus_dx2;
-                }
-                x++;
-            }
-        }
-        // Case 4: -INF < m < -1 (Mirror about x-axis and mirror 
-        // about y=x line, replace all dx by -dy and dy by dx)
-        else    
-        {
-            F = dx2 + dy;    // initial F
-
-            y = p1y;
-            x = p1x;
-            while (y >= p2y)
-            {
-		positions.push_back( envire::GridBase::Position( x, y ) );
-                if (F <= 0)
-                {
-                    F += dx2;
-                }
-                else
-                {
-                    x++;
-                    F += dy2_plus_dx2;
-                }
-                y--;
-            }
-        }
-    }
-}
 
 }
 
