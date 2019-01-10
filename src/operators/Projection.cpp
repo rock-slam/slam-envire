2b6eb96bad1e9a323f1a8da90bf0146b8c3f5376
Christoph Hertzberg <christoph.hertzberg@dfki.de>
2019-01-10 18:37:10 +0100

    Replace deprecated traits by new ones. Fixes issue #10.

--- src/operators/Projection.cpp
+++ src/operators/Projection.cpp
@@ -5,7 +5,7 @@
 #include "boost/multi_array.hpp"
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Triangulation_euclidean_traits_xy_3.h>
+#include <CGAL/Projection_traits_xy_3.h>
 #include <CGAL/Delaunay_triangulation_2.h>
 
 #include <Eigen/LU>
@@ -99,7 +99,7 @@ bool Projection::interpolateMap(const std::string& type)
     ElevationGrid::ArrayType& data(grid->getGridData(type));
 
     typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-    typedef CGAL::Triangulation_euclidean_traits_xy_3<K>  Gt;
+    typedef CGAL::Projection_traits_xy_3<K>  Gt;
     typedef CGAL::Delaunay_triangulation_2<Gt> Delaunay;
 
     typedef K::Point_3 Point;
